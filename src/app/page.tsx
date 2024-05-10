aedb36c91e7d17f3e303e769a60faf478e2502a0
Drunkss <davidessien243@gmail.com>
2024-05-09 20:04:04 -0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <div className="mx-auto max-w-7xl px-2 flex items-center justify-center">
+      <section className="flex items-center justify-center">
+
+      </section>
+    </div>
+  )
+}

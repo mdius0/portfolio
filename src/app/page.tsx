99ef4705e94964eaae1829dc2d55cbe8886d1230
Drunkss <davidessien243@gmail.com>
2024-05-09 22:41:58 -0500

    visual changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import Icons from "@/components/Icons"
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { Observer } from "gsap/Observer"
+import Image from "next/image"
+import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
 
 type fn = (a: number) => number
@@ -68,15 +71,41 @@ export default () => {
   // }, { scope: ref })
 
   return (
-    <div className="mx-auto max-w-7xl px-2 flex flex-col items-center justify-center" ref={ref}>
-      <section className="flex items-center justify-center h-screen bg-zinc-900 w-full relative">
-        <h1 className="section-heading text-white font-bold text-7xl">bruh</h1>
+    <div className="mx-auto max-w-7xl flex flex-col items-center justify-center gap-2" ref={ref}>
+      <section className="flex flex-col items-center justify-center h-screen bg-zinc-900 w-full relative p-16 gap-4">
+        <h1 className="section-heading text-white font-bold text-7xl">mdius</h1>
+
+        {/* <div className="w-full h-2 bg-white my-2"></div> */}
+
+        <div className="flex items-center justify-center gap-2">
+          <Link className="w-full bg-white text-zinc-900 font-bold text-lg hover:w-[120%] px-4 py-2 flex transtition-all duration-200 items-center justify-center gap-2" href={"https://www.github.com/mdius0"}><Image src={"/github-mark.svg"} width={25} height={25} alt="github-logo"/>Github</Link>
+          <Link className="w-full bg-white text-zinc-900 font-bold text-lg hover:w-[120%] px-4 py-2 flex transtition-all duration-200 items-center justify-center" href={"#"}>Twitter</Link>
+          <Link className="w-full bg-white text-zinc-900 font-bold text-lg hover:w-[120%] px-4 py-2 flex transtition-all duration-200 items-center justify-center" href={"#"}>Discord</Link>
+        </div>
 
         <div className="absolute w-8 h-8 bottom-2 left-2 border-l-4 border-b-4 border-l-white border-b-white"></div>
         <div className="absolute w-8 h-8 top-2 right-2 border-t-4 border-r-4 border-r-white border-t-white"></div>
       </section>
-      <section className="flex items-center justify-center h-screen bg-red-500 w-full relative">
-        <h1 className="section-heading text-zinc-900 font-bold text-7xl">Hello</h1>
+      <section className="flex flex-col items-start justify-center h-screen bg-red-500 w-full relative p-16">
+        <h1 className="section-heading text-zinc-900 font-bold text-9xl">About me</h1>
+
+        <div className="w-full h-1/2 flex items-center justify-center">
+          <div className="h-full w-full flex flex-col items-center justify-center gap-2">
+            <div className="h-full w-full bg-zinc-900"></div>
+            <div className="flex w-full h-4 items-center justify-between gap-2">
+              <div className="w-full hover:w-[150%] transition-all duration-200 h-full bg-zinc-800"></div>
+              <div className="w-full hover:w-[150%] transition-all duration-200 h-full bg-zinc-900"></div>
+              <div className="w-full hover:w-[150%] transition-all duration-200 h-full bg-zinc-800"></div>
+              <div className="w-full hover:w-[150%] transition-all duration-200 h-full bg-zinc-800"></div>
+            </div>
+          </div>
+          <div className="h-full w-[80%] mx-4 flex flex-col items-start justify-start">
+            <h3 className="text-5xl font-bold text-zinc-900">Tab One</h3>
+            <p className="text-xl font-semibold text-zinc-800 my-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet itaque quibusdam debitis, voluptas minima laborum qui cupiditate. Totam, recusandae illum. Eos earum libero exercitationem quisquam necessitatibus totam, nobis ullam.
+            </p>
+          </div>
+        </div>
 
         <div className="absolute w-8 h-8 bottom-2 left-2 border-l-4 border-b-4 border-l-zinc-900 border-b-zinc-900"></div>
         <div className="absolute w-8 h-8 top-2 right-2 border-t-4 border-r-4 border-r-zinc-900 border-t-zinc-900"></div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default () => {
+    return (
+        <div className="max-w-7xl mx-auto flex items-center justify-center w-full z-10 fixed h-16 top-4">
+            {/* <div className="max-w-7xl mx-auto">
+
+            </div> */}
+            <div className="w-full flex items-center justify-center bg-zinc-900 border border-white h-full p-2">
+
+            </div>
+        </div>
+    )
+}
